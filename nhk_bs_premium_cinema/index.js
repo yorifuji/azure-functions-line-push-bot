7c0bf00defa75f040d58f1773e3169984a277fae
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-12-14 22:37:50 +0900

    refactoring

--- nhk_bs_premium_cinema/index.js
+++ nhk_bs_premium_cinema/index.js
@@ -24,21 +24,22 @@ if (require.main === module) {
 function main(context)
 {
     const nhk = require("./nhk_api.js");
-    let query = 
-          {
-              "area"    : "140",
-              "service" : "s3",
-	      "genre"   : "0600",
-	      "date"    : nhk.today_str()
-          };
-
+    let query = {
+        "area"    : "140",
+        "service" : "s3",
+        "genre"   : "0600",
+        "date"    : nhk.today_str()
+    };
+    
     nhk.get_program_genre(query)
         .then(format_message)
         .then(messages => {
-            context.bindings.outputQueueItem = {
-                "to"       : process.env.LINE_PUSH_TO,
-                "messages" : messages
-            };
+            if (messages.length) {
+                context.bindings.outputQueueItem = {
+                    "to"       : process.env.LINE_PUSH_TO,
+                    "messages" : messages
+                };
+            }
             context.done();
         })
         .catch(res => {
@@ -49,20 +50,20 @@ function main(context)
 
 function format_message(programs)
 {
-    var messages = programs.list.s3.map(program => {
-	let start  = new Date(program.start_time);
-	start.setTime(start.getTime() + 1000 * 60 * 60 * 9); // UTC --> JST
-	let hour   = start.getHours();
-	let minute = start.getMinutes();
-	let title  = program.title;
-	let subtitle  = program.subtitle;
-	let content = program.content;
-	const web_url = "https://goo.gl/gGyNBG";
-	return {
-	    "type" : "text",
-	    "text" : `\u{1F4FA}${title} ${hour}時${minute}分〜\n（概要）${subtitle}\n（あらすじ）${content}\n${web_url}`
-	}
+    if (!programs || !programs.list || !programs.list.s3) return [];
+
+    return programs.list.s3.map(program => {
+        let start  = new Date(program.start_time);
+        start.setTime(start.getTime() + 1000 * 60 * 60 * 9); // UTC --> JST
+        let hour      = start.getHours();
+        let minute    = start.getMinutes();
+        let title     = program.title;
+        let subtitle  = program.subtitle;
+        let content   = program.content;
+        const web_url = "https://goo.gl/gGyNBG";
+        return {
+            "type" : "text",
+            "text" : `\u{1F4FA}${title} ${hour}時${minute}分〜\n（概要）${subtitle}\n（あらすじ）${content}\n${web_url}`
+        }
     });
-    
-    return Promise.resolve(messages);
 }
