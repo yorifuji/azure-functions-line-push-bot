9715feeaa90282884d455173cd92b6e4663cfc73
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-12-14 22:47:17 +0900

    add Shared folder

--- line_webhook_endpoint/index.js
+++ line_webhook_endpoint/index.js
@@ -1,7 +1,7 @@
 
 module.exports = function(context, req) {
 
-    const line = require("./line.js");
+    const line = require("../Shared/line.js");
 
     if (line.validate_signature(req.headers['x-line-signature'], req.body)) {
 	process_request(context, req.body);
@@ -24,7 +24,7 @@ function process_request(context, body)
 	context.log(event.message);
     });
 */
-    const slack = require("./slack.js");
+    const slack = require("../Shared/slack.js");
     slack.push(process.env.SLACK_WEBHOOK_URL, { "text" : body.events[0].message.text})
         .then(console.log)
         .catch(console.log)
