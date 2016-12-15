f2adc91ba47c73fbff536d5f90385adf0ab5e79d
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-12-16 01:34:52 +0900

    プロファイル取得用の関数の追加

--- Shared/line.js
+++ Shared/line.js
@@ -3,11 +3,11 @@
   LINE API Library.
 */
 
+var https = require("https");
+var url   = require("url");
+
 function _push(message)
 {
-    var https = require("https");
-    var url   = require("url");
-
     return new Promise((resolve,reject) => {
         var post_data = JSON.stringify(message);
         var parse_url = url.parse("https://api.line.me/v2/bot/message/push");
@@ -34,9 +34,42 @@ function _push(message)
             res.on('error', err => {
                 reject(new Error(err));
             })
+        }).on('error', err => {
+            reject(new Error(err));
         });
         
         post_req.write(post_data);
+        post_req.end();
+    });
+}
+
+function _get_profile(user_id)
+{
+    return new Promise((resolve, reject) => {
+        var parse_url = url.parse(`https://api.line.me/v2/bot/profile/${user_id}`);
+        var post_options = {
+            host: parse_url.host,
+            path: parse_url.path,
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Bearer ' + process.env.LINE_CHANNEL_ACCESS_TOKEN,
+            }
+        };
+        var post_req = https.request(post_options, res => {
+            var body = "";
+            res.setEncoding('utf8');
+            res.on('data', chunk => {
+                body += chunk;
+            });
+            res.on('end', () => {
+                resolve(body);
+            })
+            res.on('error', err => {
+                reject(new Error(err));
+            })
+        }).on('error', err => {
+            reject(new Error(err));
+        });
         post_req.end();
     });
 }
@@ -54,6 +87,7 @@ function _validate_signature(signature, body)
 var line = function() {
 }
 line.push = _push;
+line.get_profile = _get_profile;
 line.validate_signature = _validate_signature;
 module.exports = line;
 
@@ -68,11 +102,11 @@ if (require.main === module) {
           {
               "to"       : process.env.LINE_PUSH_TO,
               "messages" : [
-		  {
-		      "type" : "text",
-		      "text" : "hello,line"
-		  }
-	      ]
+                  {
+                      "type" : "text",
+                      "text" : "hello,line"
+                  }
+              ]
           };
 
     line.push(message)
