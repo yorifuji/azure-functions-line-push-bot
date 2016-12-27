5a1921472c1a87b6173c2b8e5b8c7ada69802643
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-12-27 18:15:49 +0900

    fix bug

--- slack_push_queue/index.js
+++ slack_push_queue/index.js
@@ -3,6 +3,5 @@ const slack = require("../Shared/slack.js");
 
 module.exports = function (context, myQueueItem) {
     context.log('Node.js queue trigger function processed work item', myQueueItem);
-    let p = myQueueItem.map(item => slack.push(process.env.SLACK_WEBHOOK_URL, item));
-    return Promise.all(p);
+    return slack.push(process.env.SLACK_WEBHOOK_URL, myQueueItem);
 };
