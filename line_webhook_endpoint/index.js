4f604b500c54fb848b4d1d2cd77f25914cf24712
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-12-27 18:16:52 +0900

    follow/unfollowのイベントをtableに保存

--- line_webhook_endpoint/index.js
+++ line_webhook_endpoint/index.js
@@ -4,21 +4,29 @@ module.exports = function(context, req) {
     const line = require("../Shared/line.js");
 
     if (line.validate_signature(req.headers['x-line-signature'], req.body)) {
-        context.bindings.outputQueueItem = process_request(context, req.body);
+        process_request(context, req.body);
     }
     else {
         context.log('fail to validate signature');
     }
-    
+
     context.res = { body : "" };
     context.done();
 };
 
 function process_request(context, body)
 {
-    return body.events.filter(event => event.type == "message").map(msg => {
-        return {
-            "text" : msg.message.text
+    body.events.forEach(event => {
+        if ((event.type == "follow" || event.type == "unfollow") && event.source.type == "user") {
+            let record = {
+                "partitionKey" : event.type,
+                "rowKey"       : event.timestamp,
+                "userId"       : event.source.userId
+            }
+            context.bindings.outputTable = record;
+        }
+        else if (event.type == "message" && event.message.type == "text") {
+            context.bindings.outputQueueItem = {"text" : event.message.text };
         };
     });
 }
