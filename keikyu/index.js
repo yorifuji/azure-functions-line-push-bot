a0861b1df0d14a0d281a98c787fc43562dff2faf
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2017-03-31 19:38:57 +0900

    京急を追加

---
+++ keikyu/index.js
@@ -0,0 +1,59 @@
+
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    main(context);
+};
+
+if (require.main === module) {
+    var context = {
+        log : console.log,
+        done: () => {
+            console.log(context.bindings.outputQueueItem);
+        },
+        bindings : {}
+    };
+    main(context);
+}
+
+function main(context)
+{
+    var twitter = require("../Shared/twitter.js");
+    const query = 
+          {
+              "screen_name" : "keikyu_official",
+              "count"       : 20
+          };
+
+    twitter.get_timeline(query)
+        .then(filter_timeline)
+        .then(format_message)
+        .then(messages => {
+            context.bindings.outputQueueItem = {
+                "to"       : process.env.LINE_PUSH_TO,
+                "messages" : messages
+            };
+            context.done();
+        })
+        .catch(res => {
+            context.done();
+        })
+}
+
+function filter_timeline(tweets)
+{
+    tweets = tweets
+        .filter(tweet => tweet.text.indexOf('【運行情報】') )
+        .filter(tweet => (new Date() - new Date(tweet.created_at)) / 1000 <= 60 * 10);
+    return tweets.length ? Promise.resolve(tweets) : Promise.reject("no delay");
+}
+
+function format_message(tweets)
+{
+    return tweets.map(tweet => ({"type" : "text", "text" : "\u{1F683}\u{1F4A4} " + tweet.text}));
+}
