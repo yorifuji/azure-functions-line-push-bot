9a2fd0250dcbf5da027ac7420dc50fe1c358d1c6
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2017-08-31 16:37:21 +0900

    京急の電車情報を返す

--- line_webhook_endpoint/index.js
+++ line_webhook_endpoint/index.js
@@ -26,7 +26,57 @@ function process_request(context, body)
             context.bindings.outputTable = record;
         }
         else if (event.type == "message" && event.message.type == "text") {
-            context.bindings.outputQueueItem = {"text" : event.message.text };
+            if (event.message.text == "京急 のぼり") {
+                get_next_train(context, true, event.source.userId);
+            }
+            else if (event.message.text == "京急 くだり") {
+                get_next_train(context, false, event.source.userId);
+            }
+            else {
+                context.bindings.outputQueueItem = {"text" : event.message.text };
+            }
         };
     });
 }
+
+function get_next_train(context, is_up, uid)
+{
+    const line = require("../Shared/line.js");
+    const text = get_train(is_up, 10); 
+    const message = 
+          {
+              "to"       : uid,
+              "messages" : [
+                  {
+                      "type" : "text",
+                      "text" : text
+                  }
+              ]
+          };
+    
+    line.push(message)
+        .then(context.log)
+        .catch(context.log)
+}
+
+function get_train(is_up, num) {
+    
+    const keikyu_timeline = require("../Shared/keikyu_timeline.js");
+    
+    const date = new Date()
+    const base = date.getHours() * 100 + date.getMinutes()
+    
+    timeline = is_up ? keikyu_timeline.up : keikyu_timeline.down
+    let trains = []
+    timeline.forEach(tl => {
+        if (parseInt(tl.time) >= base && trains.length < num) trains.push(tl)
+    })
+    
+    let text = "";
+    trains.forEach(tr => {
+        text += `+${tr.time - base} 分後(${tr.time}) ${tr.type} ${tr.goto}行き\n`
+    })
+    return text;
+}
+
+    
