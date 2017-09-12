2b8afc0ee0e3fd4baca6b25a63e93e4c8eca24b8
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2017-09-12 11:44:19 +0900

    table

--- yahoo_rainfall/index.js
+++ yahoo_rainfall/index.js
@@ -29,40 +29,42 @@ function main(context)
     const lon = process.env.LONGITUDE;
     const lat = process.env.LATITUDE;
 
-    const last_date = null;
-    try {
-	last_date = context.bindings.inputBlob["last_date"];
-    } catch(e) {
-    }
-    if (last_date && (last_date + 1000 * 60 * 60 > new Date())) return;
-    
     yahoo.get_weather_data(api_key, lon, lat)
         .then(yahoo.get_rainfall_data)
-	.then(data => {
-	    var o = data.filter(e => e["Type"] == "observation");
-	    var f = data.filter(e => e["Type"] == "forecast"   );
-	    let tbl_data = {
-		"partitionKey"    : "rainfall",
-		"rowKey"          : Date(),
-		"date"            : o[o.length - 1]["Date"],
-		"rainfall"        : o[o.length - 1]["Rainfall"],
-		"rainfall_prev"   : o[o.length - 2]["Rainfall"],
-		"forecast"        : f[0]["Rainfall"]
-	    }
-	    context.log(tbl_data);
-	    context.bindings.rainfall = tbl_data;
-	    return data;
-	})
+        .then(data => {
+            var o = data.filter(e => e["Type"] == "observation");
+            var f = data.filter(e => e["Type"] == "forecast"   );
+            let tbl_data = {
+                "partitionKey"    : "rainfall",
+                "rowKey"          : Date(),
+                "date"            : o[o.length - 1]["Date"],
+                "rainfall"        : o[o.length - 1]["Rainfall"],
+                "rainfall_prev"   : o[o.length - 2]["Rainfall"],
+                "forecast"        : f[0]["Rainfall"]
+            }
+            context.log(tbl_data);
+            context.bindings.rainfall = tbl_data;
+            return data;
+        })
         .then(yahoo.make_rainfall_message)
+        .then(messages => {
+            const last_date = null;
+            try {
+                last_date = context.bindings.inputBlob["last_date"];
+            } catch(e) {
+            }
+            if (last_date && (last_date + 1000 * 60 * 60 > new Date())) return [];
+            else return messages;
+        })
         .then(messages => {
             if (messages.length) {
                 context.bindings.outputQueueItem = {
                     "to"       : process.env.LINE_PUSH_TO,
                     "messages" : messages
                 };
-		context.bindings.outputBlob = {
-		    "last_date" : new Date()
-		}
+                context.bindings.outputBlob = {
+                    "last_date" : new Date()
+                }
             }
             context.done();
         })
